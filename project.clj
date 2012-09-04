f484621c7b3810a426af28ac5e8a5af6e1dc9184
Jon Mulieri <jonathan.mulieri@gmail.com>
2012-09-04 00:34:34 -0400

    Using clojure 1.4 just b/c

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject tnetstrings "1.0.0-SNAPSHOT"
   :description "TNetstrings parser/encoder"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :main tnetstrings.core)
